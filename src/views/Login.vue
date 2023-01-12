3f8a695441b811fcbc9401e22c27f25e531d8294
eunv <dmsql1091@naver.com>
2023-01-12 13:33:17 +0900

    로그인 구현

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,11 @@
   <div class="black-bg">
     <div class="white-bg">
     <p class="h4 text-center mb-4">Sign in</p>
-    <label for="idIn" class="grey-text">Your id</label>
-    <input type="text" id="idIn" class="form-control" />
+    <label for="idIn" class="grey-text" >Your id</label>
+    <input type="text" id="idIn" class="form-control" v-model = "id"/>
     <br />
     <label for="pwIn" class="grey-text">Your password</label>
-    <input type="password" id="pwIn" class="form-control" />
+    <input type="password" id="pwIn" class="form-control" v-model = "password"/>
     <div class="text-center mt-4">
       <button class="btn btn-indigo" type="submit" @click="login">Login</button>
     </div>
@@ -26,7 +26,7 @@ export default {
   data() {
     return {
       id: '',
-      password:'',
+      password: '',
     }
   },
   methods: {
@@ -35,20 +35,11 @@ export default {
       firebase.auth().signInWithEmailAndPassword(self.id+'@timproject.co.kr', self.password)
           .then(() => {
             alert('로그인 완료')
-            setTimeout(() => {this.loginCheck()},1);
+            self.$router.push('/mainMap')
           })
           .catch((error) => {
             alert(error)
           })
-    },
-    loginCheck(){
-      if(this.$store.state.user != null){
-        if( this.$store.state.user.displayName === '0'){
-          this.$router.push('teacherHome')
-        }else if(this.$store.state.user.displayName === '1'){
-          this.$router.push('studentHome')
-        }
-      }
     },
   }
 }
@@ -56,13 +47,6 @@ export default {
 
 <style>
 
-.loginForm {
-  position: absolute;
-  width: 350px;
-  height: 60px;
-  left: 40%;
-  top: 30%;
-}
 .black-bg {
   width: 100%; height: 100%;
   background: rgba(0,0,0,0.5);
