5720cd9fad222076054a36f3c222c375c6f31fe2
eunv <dmsql1091@naver.com>
2023-01-18 19:02:33 +0900

    상세정보 모달창 띄우기

---
+++ src/components/Detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div v-if="modal == true" class="black-bg">
+    <div class="white-bg">
+      <button @click="$emit('closeModal')">닫기</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'detail',
+  data() {
+    return {
+    }
+  },
+
+  watch : {
+
+  },
+  props : {
+    items: Array,
+    modal : Boolean,
+
+  }
+}
+</script>
+
+<style>
+#detail{
+  position: absolute;
+  z-index : 100;
+  height: 50%;
+}
+body {
+  margin : 0;
+}
+
+.black-bg {
+  width: 50%; height: 50%;
+  background: rgba(0,0,0,0.5);
+  position: absolute; padding: 20px;
+}
+.white-bg {
+  width: 50%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+</style>
